acc2b62dc0f771fcb9b2a55758e9fb93de980c4f
Raashi7 <rashikhatri0013@gmail.com>
2021-08-24 22:02:31 +0530

    comment api changes

--- Components/Feed/CommentModal.js
+++ Components/Feed/CommentModal.js
@@ -1,25 +1,71 @@
 import React, { Component } from 'react';
-import { View, Text,Image,StyleSheet, TouchableOpacity, Dimensions, Modal } from 'react-native';
-import {serverBaseUrl, theme} from '../config';
+import { View, Text,Image,StyleSheet, TouchableOpacity, Dimensions, Modal, FlatList, TextInput } from 'react-native';
+import {serverBaseUrl, theme, dataLimit, appLogo} from '../config';
 import {Feather, AntDesign, FontAwesome} from '@expo/vector-icons';
 import CardView from '../Utils/CardView'
 import { connect } from 'react-redux'
-import {like_feed} from "../Utils/DataHelper/Feed"
+import {fetch_comments, add_comment} from "../Utils/DataHelper/Feed"
 import moment from 'moment'
-import { FlatListSlider } from '../Utils/ImageSlider';
-
 const width = Dimensions.get('window').width
 const height = Dimensions.get('window').height
 
 class CommentModal extends Component {
   state={
     modalVisible: this.props.modalVisible,
+    offset: 0,
+    loadingData: true,
+    comments: []
   }
 
 
 
   componentDidMount=()=>{
+    fetch_comments(this.props.feedId,this.state.offset,dataLimit,this.commentsCallback)
+  }
 
+  commentsCallback=(response)=>{
+      if(response.status==200)
+      {
+            response.json().then(data=>{
+                console.log(data)
+                this.setState({comments:data,loadingData:false})
+            })
+      }
+  }
+
+  add=()=>{
+    add_comment(this.state.comment, 2, this.props.feedId, 0, this.props.userInfo.id, this.addCallback)
+  }
+
+  addCallback=(response)=>{
+      if(response.status==200)
+      {
+          console.log("success",response)
+          console.log(response.map.get('location'))
+      }
+      else
+      {
+          console.log("error",response)
+      }
+  }
+
+  renderSingleComment=(item)=>{
+      return(
+          <View style={{ flexDirection: 'row', margin: 15, padding: 10}}>
+              <View>
+                  <Image source={appLogo} style={{height: 50, width: 50, borderRadius: 25}}/>
+              </View>
+              <View style={{ flexDirection: 'column'}}>
+                  <View style={{ flexDirection: 'row'}}>
+                      <Text>UserName {' • '}</Text>
+                      <Text>{moment(item.timeStamp).fromNow()}</Text>
+                  </View>
+                  <View>
+                      <Text>{item.comment}</Text>
+                  </View>
+              </View>
+          </View>
+      )
   }
 
   render() {
@@ -36,6 +82,24 @@ class CommentModal extends Component {
                         <AntDesign name="left" size={24} color="black" />
                     </TouchableOpacity>
                     <Text style={{fontFamily:'Raleway_700Bold',fontSize:20, marginLeft: 10}}>Comments</Text>
+                </View>
+                <View>
+                    <FlatList
+                        data={this.state.comments}
+                        renderItem={({item}) => this.renderSingleComment(item)}
+                        keyExtractor={(item,index)=>index}
+                    />
+                </View>
+            
+                <View style={{backgroundColor: 'white', position: 'absolute', bottom: 0, width: width*0.97, marginLeft: width*0.015}}>            
+                    <TextInput 
+                        style={{borderWidth: 1, borderColor: 'black', borderRadius:10, paddingLeft: 6, paddingBottom:30}} 
+                        onChange={(text) =>this.setState({comments: text})}
+                        placeholder="Write a Comment" placeholderTextColor='grey'>
+                    </TextInput>
+                    <TouchableOpacity style={{flexDirection: 'row',backgroundColor: theme.accentColor,paddingLeft: 8, paddingRight: 8,  paddingTop: 5, paddingBottom: 5, borderRadius: 3, marginTop: 5, alignSelf: 'center', marginBottom: 20}} onPress={()=>this.add()}>
+                        <Text style={{textAlign: 'center', fontSize: 18,color: theme.primaryColor}}>Submit</Text>
+                    </TouchableOpacity>
                 </View>
             </View>
 
--- Components/Utils/DataHelper/Feed.js
+++ Components/Utils/DataHelper/Feed.js
@@ -232,3 +232,52 @@ import mime from "mime";
                 .then((response)=>callback(response)) 
                 .catch((error)=>{console.log(error)})
             }
+
+            export const fetch_comments=(feedId,offset,dataLimit,callback)=>
+            {
+                // var formData   = new FormData(); 
+                // formData.append("fetch_banners",'true') 
+                // formData.append("offset",offset) 
+                // formData.append("data_limit",limit)  
+                let headers = new Headers(); 
+                headers.append('Content-Type', 'application/json'); 
+
+                headers.append('Access-Control-Allow-Origin', serverApiUrl);
+                headers.append('Access-Control-Allow-Credentials', 'true');
+
+                headers.append('GET', 'POST', 'OPTIONS'); 
+    
+                fetch(serverApiUrl+'feed/feed/comment/'+feedId+'/'+offset+'/'+dataLimit,
+                {
+                    method: 'GET',  
+                    headers,
+                    // body:JSON.stringify({title,description,fees,instId})
+                })
+                .then((response)=>callback(response)) 
+                .catch((error)=>{console.log(error)})
+            }
+
+
+            export const add_comment=(comment, commentor, feedId, insId, userId, callback)=>
+            {
+                // var formData   = new FormData(); 
+                // formData.append("fetch_banners",'true') 
+                // formData.append("offset",offset) 
+                // formData.append("data_limit",limit)  
+                let headers = new Headers(); 
+                headers.append('Content-Type', 'application/json'); 
+
+                headers.append('Access-Control-Allow-Origin', serverApiUrl);
+                headers.append('Access-Control-Allow-Credentials', 'true');
+
+                headers.append('GET', 'POST', 'OPTIONS'); 
+    
+                fetch(serverApiUrl+'feed/feed/comment',
+                {
+                    method: 'POST',  
+                    headers,
+                    body:JSON.stringify({comment, commentor, feedId, insId, userId})
+                })
+                .then((response)=>callback(response)) 
+                .catch((error)=>{console.log(error)})
+            }
