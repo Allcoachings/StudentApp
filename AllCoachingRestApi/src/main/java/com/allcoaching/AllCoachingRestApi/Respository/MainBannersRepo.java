d608ffe1b1abc10f2a8e437d6f78b21c90dd2db2
Cyber Flow <cyber.flow911@gmail.com>
2021-08-26 05:08:53 +0530

    api bug fixed

--- AllCoachingRestApi/src/main/java/com/allcoaching/AllCoachingRestApi/Respository/MainBannersRepo.java
+++ AllCoachingRestApi/src/main/java/com/allcoaching/AllCoachingRestApi/Respository/MainBannersRepo.java
@@ -1,7 +1,7 @@
 package com.allcoaching.AllCoachingRestApi.Respository;
 
 import com.allcoaching.AllCoachingRestApi.Entity.MainBanners;
-import jdk.tools.jmod.Main;
+ 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Modifying;
