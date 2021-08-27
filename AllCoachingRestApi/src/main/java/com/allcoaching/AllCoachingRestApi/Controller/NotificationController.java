22ceb0e0f1e4f4d4e8f736a512ac1b88ea031294
Cyber Flow <cyber.flow911@gmail.com>
2021-08-27 09:35:25 +0530

    add notification api

--- AllCoachingRestApi/src/main/java/com/allcoaching/AllCoachingRestApi/Controller/NotificationController.java
+++ AllCoachingRestApi/src/main/java/com/allcoaching/AllCoachingRestApi/Controller/NotificationController.java
@@ -1,9 +1,14 @@
 package com.allcoaching.AllCoachingRestApi.Controller;
 
+import com.allcoaching.AllCoachingRestApi.Entity.Notification;
 import com.allcoaching.AllCoachingRestApi.Service.NotificationService;
 import com.allcoaching.AllCoachingRestApi.dto.NotificationDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/api/v1/notification")
@@ -18,5 +23,13 @@ public class NotificationController {
     public Iterable<NotificationDto>  getNotification(@PathVariable long receiverId,@PathVariable int receiverType,@PathVariable int page,@PathVariable int pageSize)
     {
         return  notificationService.getNotificationFor(receiverType,receiverId,page,pageSize);
+    }
+    @CrossOrigin(origins = "*")
+    @PostMapping("/")
+    public ResponseEntity<Object> addNotification(@RequestBody Notification notification)
+    {
+        Notification notification_saved =   notificationService.saveNotification(notification);
+        URI location = ServletUriComponentsBuilder.fromPath("{id}").buildAndExpand(notification_saved.getId()).toUri();
+        return ResponseEntity.created(location).build();
     }
 }
