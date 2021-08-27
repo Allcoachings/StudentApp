0dcd2cd08eb2516091ab9332b44fcf1aaa1cdba2
Cyber Flow <cyber.flow911@gmail.com>
2021-08-27 08:21:37 +0530

    payment callback url changed

---
+++ AllCoachingRestApi/src/main/java/com/allcoaching/AllCoachingRestApi/Respository/NotificationRepo.java
@@ -0,0 +1,26 @@
+package com.allcoaching.AllCoachingRestApi.Respository;
+
+import com.allcoaching.AllCoachingRestApi.Entity.Notification;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+
+@Repository
+@Transactional
+public interface NotificationRepo extends PagingAndSortingRepository<Notification,Long> {
+
+
+    Page<Notification> findByNotificationForOrderByNotificationTimeDesc(int notificationFor, Pageable pageable);
+
+    @Modifying
+    @Query("UPDATE Notification set isSeen=:isSeen where id=:id")
+    void updateNotificationStatus(boolean isSeen,long id);
+
+
+}
+
